3cd895fd3cddb3386e3b511f0863e9a31b2ad3e7
Oleksandr <sasha2003.034@gmail.com>
2023-05-18 01:17:06 +0300

    Lab5

---
+++ Lab5/Test_Lab5_1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Test_Lab5_1
+{
+    [TestClass]
+    public class Test_Lab5_1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x;
+            int[] arr = new int[] { 14, 53, 9, 75, 11, 40 };
+
+            x = Lab5_1.Conference.longparticipant(arr);
+            Assert.AreEqual(x, 3);
+        }
+    }
+}
